d49640b91e774e37c7b830f85d3e907ec322741c
RichardWagenknecht <richardwagenknecht@icloud.com>
2025-01-01 19:33:08 -0300

    Adiciona Vosk Server

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from vosk import Model, KaldiRecognizer
+import wave
+
+app = Flask(__name__)
+model = Model("model")
+
+@app.route("/transcribe", methods=["POST"])
+def transcribe():
+    audio = request.data
+    rec = KaldiRecognizer(model, 16000)
+
+    if rec.AcceptWaveform(audio):
+        return rec.Result()
+    return rec.PartialResult()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
